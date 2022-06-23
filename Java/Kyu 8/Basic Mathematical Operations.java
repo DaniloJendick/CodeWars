af125ce4d83842b86dd3e7e31f65cc629ebf518a
DaniloJendick <42503051+DaniloJendick@users.noreply.github.com>
2022-06-23 12:27:41 +0100

    Create Basic Mathematical Operations.java

---
+++ Java/Kyu 8/Basic Mathematical Operations.java
@@ -0,0 +1,13 @@
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    return 
+      (op == "-") ? v1 - v2 :
+      (op == "+") ? v1 + v2 :
+      (op == "*") ? v1 * v2 :
+       v1 / v2;
+      
+      
+  }
+}
